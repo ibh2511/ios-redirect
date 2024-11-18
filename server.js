d29b985f17ad311263905251dc962cd416daf26c
ibh2511 <84977069+ibh2511@users.noreply.github.com>
2024-11-18 11:00:21 +0100

    Update server.js
    window.location.href = `x-safari-${url}`;

--- server.js
+++ server.js
@@ -3,38 +3,31 @@ const app = express();
 
 app.get('*', (req, res) => {
     const userAgent = req.headers['user-agent'] || '';
-    const isFacebookApp = /FBAV|FBAN/i.test(userAgent); // Check if the request comes from Facebook App
-    const isIOS = /iPhone|iPad|iPod/i.test(userAgent); // Check if the device is iOS
+    const isFacebookApp = /FBAV|FBAN/i.test(userAgent); // Sjekker om det er Facebook App
+    const isIOS = /iPhone|iPad|iPod/i.test(userAgent); // Sjekker om det er iOS
+    const url = 'https://script.google.com/macros/s/AKfycbwrh7hhJioUbGhkAnqTlnEgROgDOSuqZNGUFEbDmtyAFM45uWsfaGaHgcaWdl-gCOvZ/exec'
 
     if (isFacebookApp && isIOS) {
-        // Serve a page that forces the link to open in Safari
+        // Tving Safari-åpning via mellomside
         res.send(`
-            <!DOCTYPE html>
             <html>
-            <head>
-                <title>Redirecting...</title>
-                <meta http-equiv="refresh" content="0; url=https://script.google.com/macros/s/AKfycbwrh7hhJioUbGhkAnqTlnEgROgDOSuqZNGUFEbDmtyAFM45uWsfaGaHgcaWdl-gCOvZ/exec">
-                <script>
-                    // Ensure the link opens in Safari if the meta refresh fails
-                    setTimeout(() => {
-                        window.location.replace("https://script.google.com/macros/s/AKfycbwrh7hhJioUbGhkAnqTlnEgROgDOSuqZNGUFEbDmtyAFM45uWsfaGaHgcaWdl-gCOvZ/exec");
-                    }, 1000);
-                </script>
-            </head>
-            <body>
-                <noscript>
-                    <p>Redirecting to <a href="https://script.google.com/macros/s/AKfycbwrh7hhJioUbGhkAnqTlnEgROgDOSuqZNGUFEbDmtyAFM45uWsfaGaHgcaWdl-gCOvZ/exec">Juletreff 2024</a>...</p>
-                </noscript>
-            </body>
+                <head>
+                    <title>Redirecting...</title>
+                </head>
+                <body>
+                    <script>
+                        window.location.href = `x-safari-${url}`;
+                    </script>
+                </body>
             </html>
         `);
     } else {
-        // Directly redirect to the URL for all other cases
-        res.redirect('https://script.google.com/macros/s/AKfycbwrh7hhJioUbGhkAnqTlnEgROgDOSuqZNGUFEbDmtyAFM45uWsfaGaHgcaWdl-gCOvZ/exec');
+        // Vanlig omdirigering
+        res.redirect(url);
     }
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on port ${port}`);
 });
